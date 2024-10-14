0bb3c416f0ebcdf055d319bc4748db4b860f8c3a
galibkhan1 <vampboy876@gmail.com>
2024-10-15 02:06:44 +0530

    fix

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,80 +1,98 @@
-const {User}=require("../models.js/user")
-const jwt=require("jsonwebtoken");
-const SECRET="scret"
+const { User } = require("../models.js/user");
+const jwt = require("jsonwebtoken");
+const SECRET = "scret";
 
-const signup=async(req,res)=>{
-    //body extract 
+const signup = async (req, res) => {
+    try {
+        // Body extraction
+        const { name, dob, password, email } = req.body;
 
-    //check user exists or not 
-
-    //password hashing
-
-    // user register
-
-    const {name,dob,password,email}=req.body;
-
-    const existingUser=await User.findOne({email});
-
-    if(existingUser){
-        return res.status(300).json({
-            "error":"User already registered"
-        })
-    }
-
-    const createdUser=await User.create({
-        name,
-        email,
-        dob,
-        password
-    })
-
-    const token = await jwt.sign({ name, email }, SECRET);
-
-
-    return res.status(200).json({
-        "name":createdUser.name,
-        "email":createdUser.email,
-        "access_token":token
-    })
-
-}
-
-const signin =async(req,res)=>{
-    const {password,email} = req.body;
-    const existingUser = await User.findOne({email});
-    if(!existingUser){
-        return res.status(404).json({
-            "error":"User Not exist"
-        })
-    }
-    
-
-    if(existingUser.password !== password){
-        return res.status(400),json({
-            "error":"Password is not valid"
-        })
-    }
-    const name=existingUser.name;
-    const token = await jwt.sign({name , email }, SECRET);
-    return res.status(200).json({
-        "message":"User loggedIn",
-        "user":{
-            "name":existingUser.name,
-            "email":existingUser.email,
-            "access_token":token
+        // Check if user exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({
+                "error": "User already registered"
+            });
         }
-    })
-}
-const getAllUser = async(req,res)=>{
-    const users = await User.find({});
-    return res.status(200).json({
-        "message":"Users fetched successfully",
-        "users":users
-    })
-}
 
-module.exports={
+        // User registration
+        const createdUser = await User.create({
+            name,
+            email,
+            dob,
+            password
+        });
+
+        // Generate token
+        const token = await jwt.sign({ name, email }, SECRET);
+
+        return res.status(201).json({
+            "name": createdUser.name,
+            "email": createdUser.email,
+            "access_token": token
+        });
+    } catch (error) {
+        console.error("Error during signup:", error);
+        return res.status(500).json({
+            "error": "Internal server error"
+        });
+    }
+};
+
+const signin = async (req, res) => {
+    try {
+        const { password, email } = req.body;
+        const existingUser = await User.findOne({ email });
+
+        if (!existingUser) {
+            return res.status(404).json({
+                "error": "User does not exist"
+            });
+        }
+
+        // Check password (this should ideally use a hashed password check)
+        if (existingUser.password !== password) {
+            return res.status(400).json({
+                "error": "Invalid password"
+            });
+        }
+
+        const name = existingUser.name;
+        const token = await jwt.sign({ name, email }, SECRET);
+
+        return res.status(200).json({
+            "message": "User logged in",
+            "user": {
+                "name": existingUser.name,
+                "email": existingUser.email,
+                "access_token": token
+            }
+        });
+    } catch (error) {
+        console.error("Error during signin:", error);
+        return res.status(500).json({
+            "error": "Internal server error"
+        });
+    }
+};
+
+const getAllUser = async (req, res) => {
+    try {
+        const users = await User.find({});
+        return res.status(200).json({
+            "message": "Users fetched successfully",
+            "users": users
+        });
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        return res.status(500).json({
+            "error": "Internal server error"
+        });
+    }
+};
+
+module.exports = {
     signup,
     signin,
     getAllUser
-}
+};
