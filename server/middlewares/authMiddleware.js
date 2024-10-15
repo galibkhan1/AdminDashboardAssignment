21416fa61069ce108d8df7ee1331fb959abe1df0
Mohd Galib <95847361+galibkhan1@users.noreply.github.com>
2024-10-15 08:32:36 +0530

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,24 +1,23 @@
 const jwt = require("jsonwebtoken");
-const SECRET = "scret"; // Make sure to correct the typo "scret" to "secret"
+const SECRET = "scret";
 
 const authMiddleWare = async (req, res, next) => {
-    const token = req.headers.authorization && req.headers.authorization.split(" ")[1]; // Extract the token from "Bearer <token>"
+    const token = req.headers.authorization && req.headers.authorization.split(" ")[1]; 
 
     if (!token) {
-        return res.status(401).json({ error: "Unauthorized access" }); // Return 401 if no token is provided
+        return res.status(401).json({ error: "Unauthorized access" });
     }
 
     console.log(token)
 
     try {
         
-        const isVerified = jwt.verify(token, SECRET); // Verify the token
+        const isVerified = jwt.verify(token, SECRET);
         if(isVerified){
             next();
-        }; // Optionally, attach the decoded user info to the request object
-        // next(); // Call the next middleware or route handler
+        }; 
     } catch (err) {
-        return res.status(401).json({ error: "Unauthorized access" }); // Return 401 if token is invalid
+        return res.status(401).json({ error: "Unauthorized access" }); 
     }
 };
 
